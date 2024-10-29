61ad85d7dc921c5487d9ad497156b216bbee6894
Muhammad Yusri <135690839+muhammadyusri121@users.noreply.github.com>
2024-10-30 03:36:21 +0700

    update modul 4

---
+++ modul-4/23_008 Muhammad Yusri/praktikum4/src/praktikum4/Praktikum4.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package praktikum4;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Praktikum4 {
+
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int akhir =100;
+    int awal =1;
+    int random;
+//    int simpan = random;
+        
+        
+        
+        
+    random = awal + (int)(Math.random()*((akhir-awal)+1));
+        System.out.println(random);
+    }
+    
+}
